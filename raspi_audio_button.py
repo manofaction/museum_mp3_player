7e45b43884a365cfca965241235c63d44fe345d7
manofaction <manofaction@users.noreply.github.com>
2016-04-13 08:26:31 -0500

    Add buttons/tracks and wait time, nest button operations in loop

--- raspi_audio_button.py
+++ raspi_audio_button.py
@@ -1,20 +1,25 @@
 #!usr/bin/env python
 
 import os
-from time import sleep
-
 import RPi.GPIO as GPIO
 
+track_name_list = ['let_it_go.mp3', 'home.mp3', 'generations.mp3', 'coming_soon.mp3']
+track_length_list = [227, 261, 202, 3]
+track_path = "/home/pi/Desktop/museum_mp3_player_project/mp3_files/"
+pin_list = [23, 24, 17, 27, 22]
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(23, GPIO.IN)
-GPIO.setup(24, GPIO.IN)
+
+for pin in pin_list:
+	GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+time_current_track = 0
+time_started = 0
 
 while True:
-    if (GPIO.input(23)==False):
-        os.system('mpg123 -q mp3_files/track_1_test.mp3')
-
-    if (GPIO.input(24)==False):
-        os.system('mpg123 -q mp3_files/track_2_test.mp3')
-
-    sleep(1);
+	for button_number in 0:length(track_name_list):
+		if GPIO.input(pin_list[button_number])==False and time.time() - time_started > time_current_track:
+			time_current_track = track_length_list[button_number]
+			time_started = time.time()
+			os.system('mpg123 -q %s%s' % (track_path, track_name_list[button_number])
+    time.sleep(0.2)
